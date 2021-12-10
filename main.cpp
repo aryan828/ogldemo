c949a08e3b9057d644d2780f05a8636512bacedf
Aryan Narayan <aryannarayans@gmail.com>
2021-12-11 05:02:20 +0530

    Draw triangles or square; added log function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,166 @@
 
 #include <iostream>
 
+void log(const std::string& message) {
+    time_t now = time(nullptr);
+    tm* localtm = localtime(&now);
+    char buffer[80];
+    strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", localtm);
+    std::cout << buffer << ": " << message << std::endl;
+}
+
+class Renderer {
+    const char *vertexShaderSource = "#version 460 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+    const char *fragmentShaderSource = "#version 460 core\n"
+                                       "out vec4 FragColor;\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\0";
+
+    unsigned int vertexShader;
+    unsigned int fragmentShader;
+    unsigned int shaderProgram;
+
+public:
+    Renderer() {
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+        glCompileShader(vertexShader);
+        checkCompileErrors(vertexShader, "VERTEX");
+        log("Vertex shader compiled");
+
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+        glCompileShader(fragmentShader);
+        checkCompileErrors(fragmentShader, "FRAGMENT");
+        log("Fragment shader compiled");
+
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        checkCompileErrors(shaderProgram, "PROGRAM");
+        log("Shader program compiled");
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    void use() const {
+        glUseProgram(shaderProgram);
+    }
+
+    static void checkCompileErrors(unsigned int shader, const std::string& type) {
+        int success;
+        char infoLog[1024];
+        if (type != "PROGRAM") {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+                log(std::string("ERROR::SHADER_COMPILATION_ERROR of type: ") + type + "\n" + infoLog);
+            }
+        } else {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shader, 1024, nullptr, infoLog);
+                log("ERROR::PROGRAM_LINKING_ERROR of type: " + type + "\n" + infoLog + "\n -- --------------------------------------------------- -- ");
+            }
+        }
+    }
+
+    ~Renderer() {
+        glDeleteProgram(shaderProgram);
+    }
+};
+
+class Triangle {
+    float vertices[9] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VAO;
+    unsigned int VBO;
+
+public:
+    Triangle() {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        log("Triangle initialized");
+    }
+
+    void draw() const {
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+//        glBindVertexArray(0);
+    }
+
+    ~Triangle() {
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+    }
+};
+
+class Square {
+    float vertices[12] = {
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+    };
+    unsigned int indices[6] = {  // note that we start from 0!
+            0, 1, 3,  // first Triangle
+            1, 2, 3   // second Triangle
+    };
+
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+
+    public:
+    Square() {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        log("Square initialized");
+    }
+
+    void draw() const {
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+//        glBindVertexArray(0);
+    }
+};
 
 class Window {
     unsigned int width;
@@ -24,6 +184,8 @@ class Window {
             std::cout << "Failed to create GLFW window" << std::endl;
             glfwTerminate();
             return;
+        } else {
+            log("Window created");
         }
         glfwMakeContextCurrent(window);
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -31,14 +193,21 @@ class Window {
         if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
             std::cout << "Failed to initialize GLAD" << std::endl;
             return;
+        } else {
+            log("GLAD initialized");
         }
     }
 
-    void run() const {
+    void run(const Renderer& renderer) const {
+//        Triangle triangle;
+        Square square;
         while (!glfwWindowShouldClose(window)) {
             processInput(window);
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
+            renderer.use();
+//            triangle.draw();
+            square.draw();
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
@@ -61,7 +230,8 @@ class Window {
 int main() {
     Window window;
     window.create();
-    window.run();
+    Renderer renderer;
+    window.run(renderer);
 
     return 0;
 }
